f6a43ec6b0fa3c1cdd4e39645c0b35c6fa598bd8
Matthew Malensek <matt@malensek.net>
2022-05-04 23:33:12 -0700

    Add find test to 12

--- 12-Unix-Utilities-1.sh
+++ 12-Unix-Utilities-1.sh
@@ -6,8 +6,10 @@ test_start "Unix Utilities" \
 # Check to make sure the library exists
 [[ -e "./allocator.so" ]] || test_end 1
 
-LD_PRELOAD=./allocator.so   df      || test_end
-LD_PRELOAD=./allocator.so   w       || test_end
-LD_PRELOAD=./allocator.so   ps aux  || test_end
+LD_PRELOAD=./allocator.so   df                     || test_end
+LD_PRELOAD=./allocator.so   w                      || test_end
+LD_PRELOAD=./allocator.so   ps aux                 || test_end
+LD_PRELOAD=./allocator.so   find /etc &> /dev/null || test_end
+                           # The Daniel Test (c) 2021 Daniel Barajas
 
 test_end
